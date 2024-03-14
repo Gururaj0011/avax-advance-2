f88a06b8ebe018f2d171902b600612f3a568c6f4
Gururaj0011 <158799413+Gururaj0011@users.noreply.github.com>
2024-03-14 22:59:52 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "gururaj"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = " guru"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "GR"
 )
 
 var ID ids.ID
